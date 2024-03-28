a93aa48d0f20fbf6ee167504bebd7342d6cf2840
Nguyen Viet Viet Anh <anhnvv.work@gmail.com>
2024-03-28 21:48:15 +0700

    add sth

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan")
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 
 let reqCnt = 0
 app.get("/", (req, res) => {
   reqCnt += 1;
   console.log(`resCnt ${reqCnt}`);
-  res.send("<h1>API is running...<h1>");
+  res.send(`
+    <h1>API is running...<h1>
+    <h2>Counter: ${reqCnt}<h2>
+  `);
 })
 
 PORT = 5000
